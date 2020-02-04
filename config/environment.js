1889434e09c7fb4a855a069bea549e1c3677af42
cara <xw33@outlook.com>
2020-02-04 11:45:40 +0000

    typo *mongoddb

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 
 const port = process.env.PORT || 4000
-const dbURI = process.env.MONGODDB_URI || 'mongodb://localhost/restful-api'
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/restful-api'
 const secret = process.env.SECRET || 'open sesame'
 
 module.exports = { port, dbURI, secret }
